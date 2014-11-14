42576af7c8da24acd3ae6c14e479b8d0e1f374ef
CaioMarinho <CaioMarinho@users.noreply.github.com>
2014-11-13 22:42:26 -0200

    Create Empresa.java
    Rascunho da classe empresa

---
+++ java/Empresa.java
@@ -0,0 +1,25 @@
+
+public class Empresa {
+	
+	private String nome;
+	private int cnpj;
+	
+	float eviar_consumo (float f) {
+		return f;
+	}
+	
+	void adicionar_evento () {
+		
+	}
+	
+	void remover_evento () {
+		
+	}
+	
+	void editar_evento () {
+		
+	}
+	
+	
+
+}
