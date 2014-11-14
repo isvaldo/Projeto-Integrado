688782b6c269c2b2433997d12a15ea30e52aadcd
CaioMarinho <CaioMarinho@users.noreply.github.com>
2014-11-14 15:23:46 -0200

    Create Evento.java
    Mais uma, verifiquem se é isso mesmo e mudem o que precisar.

---
+++ java/Evento.java
@@ -0,0 +1,31 @@
+
+public class Evento {
+	
+	private String nome, descricao;
+	private String participantes;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setParticipantes(String participantes) {		
+		this.participantes = participantes;
+	}
+	
+	public String getParticipantes() {
+		return participantes;
+	}
+
+}
