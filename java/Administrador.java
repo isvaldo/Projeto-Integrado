db950012b5be88350cb802732208d6231416cb46
CaioMarinho <CaioMarinho@users.noreply.github.com>
2014-11-14 15:26:53 -0200

    Create Administrador.java
    Mais uma

---
+++ java/Administrador.java
@@ -0,0 +1,22 @@
+
+public class Administrador {
+	
+	private String nome;
+	
+	public void adicionar_Empresa() {
+		
+	}
+	
+	public void remover_Empresa() {
+		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+}
